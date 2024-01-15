da31ddcadad95e8bfb5ea8e60b83edb243bb3630
Monkey <156485972+monovico@users.noreply.github.com>
2024-01-15 17:38:59 -0600

    sounds works now

--- voice_talk_gtts.py
+++ voice_talk_gtts.py
@@ -1,24 +1,29 @@
 from gtts import gTTS
 import os
 import pyttsx3
-from playsound import playsound
+from pydub import AudioSegment
+from pydub.playback import play
 
-text = "Hello! My name is Sebastian.a little silly"
+# updated with PyDub, now the sounds work
+
+text = "Hello! My name is Mono Vico. I am a little silly"
 tts = gTTS(text, lang='en')
 tts.save("hi.mp3")
-playsound('hi.mp3')
+sound = AudioSegment.from_mp3('hi.mp3')
+play(sound)
 os.remove('hi.mp3')
 
-text = "Hola, me llamo Sebastian. Un poco loco"
+text = "Hola, me llamo Mono Vico. Un poco loco"
 tts = gTTS(text, lang='es')
 tts.save("hi2.mp3")
-playsound('hi2.mp3')
+sound = AudioSegment.from_mp3('hi2.mp3')
+play(sound)
 os.remove('hi2.mp3')
 
 
 engine = pyttsx3.init()
-engine.say("Hello! My name is Sebastian a little silly")
+engine.say("Hello! My name is Vico a little silly")
 engine.runAndWait()
 engine = pyttsx3.init()
-engine.say("ola! mee yamoe Sbastean oon pohcoh lohcoh")
+engine.say("ola! mee yamoe Vicoooo oon pohcoh lohcoh")
 engine.runAndWait()
